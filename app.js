e17153610aad6bf8ed7acde6db24a80a778c7efd
aimeeguzman <guzman.aimee@gmail.com>
2016-03-04 09:32:12 -0800

    created empty array and function with array of names to reset names

---
+++ app.js
@@ -0,0 +1,7 @@
+var codetalkNames = [];
+
+function resetNames(argument) {
+	// body...
+	codtalkNames = ['Bernice', 'Elliette', 'Mirna', 'Erinn', 'Aimee', 'Yadira', 'Jessica', 'Janene', 'Denise'];
+	
+}
